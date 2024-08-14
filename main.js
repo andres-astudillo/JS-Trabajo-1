f51c6a4d48d19cfc9c7cec801a1156cc807f00f8
Andres Astudillo <andresdiastullo@gmail.com>
2024-08-13 23:44:45 -0300

    Final

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+let inventario = [];
+
 function agregarProductos() {
-  const inventario = [];
   let producto = prompt("Ingresa un producto para inventariar");
   inventario.push(producto);
 }
 
 function imprimirListado() {
+  const tbody = document.getElementById("tbody-inventario");
+  tbody.innerHTML = ""; // Limpia la tabla antes de agregar nuevos productos
   for (const producto of inventario) {
-    console.log(producto);
+    const fila = `
+      <tr>
+        <td>${producto}</td>
+      </tr>
+    `;
+    tbody.innerHTML += fila;
   }
 }
 
+let respuesta;
 do {
   agregarProductos();
-  let respuesta = prompt("¿Desea agregar otro producto? (s/n)");
+  respuesta = prompt("¿Desea agregar otro producto? (s/n)");
 } while (respuesta.toLowerCase() === "s");
 
 imprimirListado();
